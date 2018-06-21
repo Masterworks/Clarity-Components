4524235b4be9fa01d3c4025f9fb60b13a50d8d4e
“Chitradip.capitalnumbers” <chitradip@capitalnumbres.com>
2018-06-21 16:28:12 +0530

    custom.js added

---
+++ clarity-html/scripts/custom.js
@@ -0,0 +1,24 @@
+//notification start
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.btn-notifications')) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+//notification end
+
+$(".btn-user-account").click(function () {
+    $(".js-signout").toggleClass("active");
+});
