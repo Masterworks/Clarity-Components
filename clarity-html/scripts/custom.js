ee2df8121c55ee010fb7ed3cc3259dfa70f88369
Daniel Golden <danielgolden90@gmail.com>
2018-07-09 10:40:03 -0400

    Refinements to the edit projects page
    - js for properly positioning label when option has been chosen
    - Updates to position of input and select labels
    - Removes some old, now unused, css

--- clarity-html/scripts/custom.js
+++ clarity-html/scripts/custom.js
@@ -69,3 +69,28 @@ $('#filetype').on('change', function () {
         $('#urltype').show();
     }
 });
+
+// Check for inputs that have value and apply class to handle label
+// styling so that text isn't covered by label. Commented out
+// because I found a way to do this with CSS, but maybe this could
+// resolve bugs related to browser and password manager autofill
+// work in progress.
+
+// $('input[type="text"], input[type="email"]').each(function() {
+//     if($(this).val().length != 0) {
+//         $(this).siblings('label').addClass('input-has-text');
+//     } else {
+//         $(this).siblings('label').removeClass('input-has-text');
+//     }
+// });
+
+
+// Check for Selects that have an option selected already and 
+// apply class to handle label styling
+
+$('select').find(':selected').each(function() {
+    if($(this).val() != '') {
+        console.log($(this));
+        $(this).parents('.c-select').addClass('input-has-text');
+    }
+});
